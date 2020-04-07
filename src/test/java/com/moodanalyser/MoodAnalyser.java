84351fd374aca14c91bbec11f38a4667510c600e
Priyankateke <priiyankateke1705@gmail.com>
2020-04-07 08:51:57 -0700

    REFACTORED: Passed Message To Constructor

--- src/test/java/com/moodanalyser/MoodAnalyser.java
+++ src/test/java/com/moodanalyser/MoodAnalyser.java
@@ -1,11 +1,23 @@
 package com.moodanalyser;
 
 public class MoodAnalyser {
+    private String message;
+
+    /* Default Constructor*/
+    public MoodAnalyser() {
+    }
+
+    /* Parameterized Constructor*/
+    public MoodAnalyser(String message) {
+        this.message=message;
+    }
 
     /* If Message contains "Sad" returns "Sad" else return Happy */
-    public String analyseMood(String message) {
+    public String analyseMood() {
         if(message.contains("Sad"))
             return "Sad";
-        return "Happy";
+        else if(message.contains("Happy"))
+            return "Happy";
+        return null;
     }
 }
--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -6,15 +6,17 @@ import org.junit.Test;
 public class MoodAnalyserTest {
     MoodAnalyser moodAnalyser=new MoodAnalyser();
 
-    /* TC 1.1 : Given I am in Sad Mood message Should Return SAD */
+    /* TC 1.1 : Given I am in Sad Mood message in Constructor Should Return Sad */
     @Test
     public void givenMood_WhenSad_ShouldReturnSad() {
-        Assert.assertEquals("Sad",moodAnalyser.analyseMood("I am in Sad Mood"));
+        MoodAnalyser moodAnalyser=new MoodAnalyser("I am in Sad Mood");
+        Assert.assertEquals("Sad",moodAnalyser.analyseMood());
     }
 
-    /* T.C. 1.2 : Given I am in Any Mood message Should Return HAPPY */
+    /* T.C. 1.2 : Given I am in Happy Mood message in Constructor Should Return Happy */
     @Test
     public void givenMood_WhenHappy_ShouldReturnHappy() {
-        Assert.assertEquals("Happy",moodAnalyser.analyseMood("I am in any Mood"));
+        MoodAnalyser moodAnalyser=new MoodAnalyser("I am in Happy Mood");
+        Assert.assertEquals("Happy",moodAnalyser.analyseMood());
     }
 }
